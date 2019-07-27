fbf02af587bb5231fcd750d29f6eb5951b515dc6
danashviro <danashviro@gmail.com>
2019-07-27 12:53:57 +0300

    add icon and add mark and settings page for IOS

--- iOS/NewMarkViewController.designer.cs
+++ iOS/NewMarkViewController.designer.cs
@@ -14,8 +14,34 @@ namespace mARkIt.iOS
     [Register ("NewMarkViewController")]
     partial class NewMarkViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        Syncfusion.iOS.Buttons.SfCheckBox generalCheckBox { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView markTextView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem saveMarkButton { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (generalCheckBox != null) {
+                generalCheckBox.Dispose ();
+                generalCheckBox = null;
+            }
+
+            if (markTextView != null) {
+                markTextView.Dispose ();
+                markTextView = null;
+            }
+
+            if (saveMarkButton != null) {
+                saveMarkButton.Dispose ();
+                saveMarkButton = null;
+            }
         }
     }
 }
